b20fa3085d1fea0018e08d9a4f537a5ae4afafe9
MARY NJOKI <marynjoki738@gmail.com>
2023-07-28 08:02:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Add your code here
+function submitData(name ="mary", email = "marynjoki738@gmail.com") {
+    const formData = {
+      name: name,
+      email: email,
+    };
+  
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(formData),
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        const id = object.id;
+        document.body.innerHTML = `User with ID ${id} has been successfully created.`;
+      })
+      .catch(function (error) {
+        document.body.innerHTML = "An error occurred while processing your request.";
+        console.log(error.message);
+      });
+  }
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    // Add event listener to form submission
+    const form = document.getElementById("userForm");
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+      const name = event.target.elements["name"].value;
+      const email = event.target.elements["email"].value;
+      submitData(name, email);
+    });
+  });
+  
